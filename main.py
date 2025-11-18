82c087cbb9f4da044d8ad996c950447be3a02ff5
Karen Alvarez <kalva088@ucr.edu>
2025-11-18 15:00:21 +0100

    1

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import os
 
+#IMPORT DATA
 #read csv file into pandas df
 df = pd.read_csv("mappings.csv")
 print("loaded dataset")
 print(df.head())
 
-#start basic cleaning
+#START BASIC CLEANING
+#replace Na values with empty strings
+df = df.fillna("")
+print("post cleaning")
+print(df.head())
+
+#COMBINE TACTICS INTO A COLUMN
+tacticColumns= ["TActic", "TA_inb", "TA_lat", "TA_out"]
+#given row, have to look at all tactic columns, return list of non empty unique tactics
+def combTactics(row):
+    tactics=[]
+    
+
+
+
 
 
 
